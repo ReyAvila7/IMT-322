5c30a6f90c0bf405a947c5142d3d1f60b9b6720d
ReyAvila7 <avilareynaldo348@gmail.com>
2024-02-24 12:02:59 -0400

    Modificación 24/02

--- sketch_feb23a/sketch_feb23a.ino
+++ sketch_feb23a/sketch_feb23a.ino
@@ -1,24 +1,56 @@
-#define LEDROJO 2
-#define LEDAMARILLO 3
-#define LEDVERDE 4
-#define TIME 200
+#define LED 13
+#define LED2 12
+#define LED3 11
+#define PULSADOR 7
+#define ARDUINO 9600
+#define TIEMPO 200
+uint8_t val=0;
+uint8_t apagar=0;
+uint8_t prender=0;
+
 void setup() {
-pinMode(LEDROJO, OUTPUT);
-pinMode(LEDAMARILLO, OUTPUT);
-pinMode(LEDVERDE, OUTPUT);
+pinMode(LED, OUTPUT);
+pinMode(LED2, OUTPUT);
+pinMode(LED3, OUTPUT);
+pinMode(PULSADOR, INPUT_PULLUP);
+Serial.begin(ARDUINO);
 }
 
 void loop() {
-  digitalWrite(LEDROJO, HIGH);
-  digitalWrite(LEDAMARILLO,LOW);
-  digitalWrite(LEDVERDE,LOW);
-  delay(TIME);
-  digitalWrite(LEDROJO, LOW);
-  digitalWrite(LEDAMARILLO,HIGH);
-  digitalWrite(LEDVERDE,LOW);
-  delay(TIME);
-  digitalWrite(LEDROJO, LOW);
-  digitalWrite(LEDAMARILLO,LOW);
-  digitalWrite(LEDVERDE,HIGH);
-  delay(TIME);
-}1
+  val=digitalRead(PULSADOR);
+  Serial.println(val);
+  if(val && apagar == 0)
+    {
+      prender = 1- prender;
+      delay (300);
+    }
+    apagar = val;
+  if (prender){
+    digitalWrite(LED, LOW);
+    digitalWrite(LED2,LOW);
+    digitalWrite(LED3,HIGH);
+    delay(TIEMPO);
+    digitalWrite(LED, LOW);
+    digitalWrite(LED2,HIGH);
+    digitalWrite(LED3,LOW);
+    delay(TIEMPO);
+    digitalWrite(LED, HIGH);
+    digitalWrite(LED2,LOW);
+    digitalWrite(LED3,LOW);
+    delay(TIEMPO);
+  }
+  else{
+    digitalWrite(LED, HIGH);
+    digitalWrite(LED2,LOW);
+    digitalWrite(LED3,LOW);
+    delay(TIEMPO);
+    digitalWrite(LED, LOW);
+    digitalWrite(LED2,HIGH);
+    digitalWrite(LED3,LOW);
+    delay(TIEMPO);
+    digitalWrite(LED, LOW);
+    digitalWrite(LED2,LOW);
+    digitalWrite(LED3,HIGH);
+    delay(TIEMPO);
+  }
+}
